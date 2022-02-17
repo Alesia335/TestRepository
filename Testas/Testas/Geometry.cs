5d0f011671896b35259ebaa9f29e58806eae6063
Alesia335 <alesia.kov@gmail.com>
2022-02-17 22:11:12 +0200

    Class

---
+++ Testas/Testas/Geometry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testas
+{
+    internal class Geometry
+    {
+        public int firstVar;
+        public int secondVar;
+        public int thirdVar;
+
+        public void Task1 
+        { 
+
+        }
+        public void Task2
+        {
+
+        }
+        public void Task3
+        {
+
+        }
+    }
+}
